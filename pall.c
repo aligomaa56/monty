099f663541b64c51d667675d45e14aaaf2f26794
aligomaa56 <3looosh56565656@gmail.com>
2023-12-15 02:09:31 +0200

    done

---
+++ pall.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * f_pall - print stack
+ * @head: head
+ * @counter: nothing
+ * Return: void
+*/
+void f_pall(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+	if (h == NULL)
+		return;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+}
